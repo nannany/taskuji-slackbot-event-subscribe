c91652b79d2a850ae2c0158c937873715c954501
minami.yoshihiko <nannany@st.inc>
2024-04-28 19:50:12 +0900

    ignore explicitlyしたけども

--- slashHandler.go
+++ slashHandler.go
@@ -47,7 +47,7 @@ func (h slashHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		if len(strings.Trim(s.Text, "")) == 0 {
 			members, err := h.memberCollector.Collect(s.ChannelID)
 			if err != nil {
-				w.Write([]byte("require invite bot to this channel if here is not public channel"))
+				_, _ = w.Write([]byte("require invite bot to this channel if here is not public channel"))
 				return
 			}
 			h.lot.DrawLots(s.ChannelID, members, "")
@@ -57,19 +57,19 @@ func (h slashHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		idReg := regexp.MustCompile(`\^([^|>]+)`)
 		idMatch := idReg.FindAllStringSubmatch(strings.Trim(s.Text, ""), -1)
 		if !(len(idMatch) > 0 && len(idMatch[0]) > 1) {
-			w.Write([]byte("can not find usergroup.  This usually works: /gacha [@usergroup]"))
+			_, _ = w.Write([]byte("can not find usergroup.  This usually works: /gacha [@usergroup]"))
 			return
 		}
 		ugID := idMatch[0][1]
 		members, err := h.memberCollector.CollectByUserGroup(ugID, s.ChannelID)
 		if err != nil {
-			w.Write([]byte("require invite bot to this channel if here is not public channel"))
+			_, _ = w.Write([]byte("require invite bot to this channel if here is not public channel"))
 			return
 		}
 		groupNameReg := regexp.MustCompile(`@(.+)>`)
 		groupNameMatch := groupNameReg.FindAllStringSubmatch(strings.Trim(s.Text, ""), -1)
 		if !(len(groupNameMatch) > 0 && len(groupNameMatch[0]) > 1) {
-			w.Write([]byte("can not extract usergroup name.  This usually works: /gacha [@usergroup]"))
+			_, _ = w.Write([]byte("can not extract usergroup name.  This usually works: /gacha [@usergroup]"))
 			log.Print(s.Text)
 			return
 		}
@@ -78,9 +78,9 @@ func (h slashHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		bodyMatch := bodyReg.FindAllStringSubmatch(strings.Trim(s.Text, ""), -1)
 		if len(bodyMatch) > 0 && len(bodyMatch[0]) > 1 {
 			body := bodyMatch[0][1]
-			h.slackClient.PostMessage(s.ChannelID, slack.MsgOptionText(fmt.Sprintf("%s draw gacha @%s %s", s.UserName, groupName, body), true))
+			_, _, _ = h.slackClient.PostMessage(s.ChannelID, slack.MsgOptionText(fmt.Sprintf("%s draw gacha @%s %s", s.UserName, groupName, body), true))
 		} else {
-			h.slackClient.PostMessage(s.ChannelID, slack.MsgOptionText(fmt.Sprintf("%s draw gacha @%s", s.UserName, groupName), true))
+			_, _, _ = h.slackClient.PostMessage(s.ChannelID, slack.MsgOptionText(fmt.Sprintf("%s draw gacha @%s", s.UserName, groupName), true))
 		}
 
 		h.lot.DrawLots(s.ChannelID, members, ugID)
